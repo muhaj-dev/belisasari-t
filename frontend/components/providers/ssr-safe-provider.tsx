822fda47eff0b6f5b4f1db7f609219c89840caf8
Afoxcute <jwavolabilove16@gmail.com>
2026-02-23 20:01:56 +0100

    fixed privy deployment env v18

--- frontend/components/providers/ssr-safe-provider.tsx
+++ frontend/components/providers/ssr-safe-provider.tsx
@@ -1,20 +1,12 @@
 'use client';
 
-import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 import { EnvironmentStoreProvider } from '@/components/context';
 import SolanaWalletProvider from './wallet-provider';
 import { ThemeProvider } from './theme-provider';
+import { PrivyClientProvider } from '@/components/provider/PrivyClientProvider';
 import Layout from '@/components/sections/layout';
 import { Toaster } from '@/components/ui/toaster';
-
-const PrivyClientProvider = dynamic(
-  () =>
-    import('@/components/provider/PrivyClientProvider').then((m) => ({
-      default: m.PrivyClientProvider,
-    })),
-  { ssr: false }
-);
 
 export default function SSRSafeProvider({ children }: { children: React.ReactNode }) {
   const [isClient, setIsClient] = useState(false);
@@ -52,14 +44,23 @@ export default function SSRSafeProvider({ children }: { children: React.ReactNod
       enableSystem={false}
       disableTransitionOnChange
     >
-      <PrivyClientProvider>
+      {PrivyClientProvider ? (
+        <PrivyClientProvider>
+          <EnvironmentStoreProvider>
+            <SolanaWalletProvider>
+              <Layout>{children}</Layout>
+              <Toaster />
+            </SolanaWalletProvider>
+          </EnvironmentStoreProvider>
+        </PrivyClientProvider>
+      ) : (
         <EnvironmentStoreProvider>
           <SolanaWalletProvider>
             <Layout>{children}</Layout>
             <Toaster />
           </SolanaWalletProvider>
         </EnvironmentStoreProvider>
-      </PrivyClientProvider>
+      )}
     </ThemeProvider>
   );
 }
